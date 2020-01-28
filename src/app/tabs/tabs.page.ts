da0d333064b8a7884d151a5b23aad630d3fccec2
Boaz Yaari <boazy@mprest.com>
2020-01-28 20:28:59 +0200

    new design

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -6,7 +6,7 @@ import {Component} from '@angular/core';
     styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
-    isDashbord = true;
+    isDashboard = true;
     isWeapon = false;
     isSettings = false;
 
@@ -14,19 +14,19 @@ export class TabsPage {
     }
 
     onTabOneClick() {
-        this.isDashbord = true;
+        this.isDashboard = true;
         this.isSettings = false;
         this.isWeapon = false;
     }
 
     onTabTwoClick() {
-        this.isDashbord = false;
+        this.isDashboard = false;
         this.isSettings = false;
         this.isWeapon = true;
     }
 
     onTabThreeClick() {
-        this.isDashbord = false;
+        this.isDashboard = false;
         this.isSettings = true;
         this.isWeapon = false;
     }
